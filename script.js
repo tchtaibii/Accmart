6699ac5b02e325a461b6f0c8260c12c8cd636e01
Taha Chtaibi <tchtaibi@e2r4p2.1337.ma>
2022-10-25 17:23:31 +0100

    scroll

---
+++ script.js
@@ -0,0 +1,15 @@
+function ft_sc(value, svg)
+{
+    var p = document.getElementById(value);
+    var svg = document.getElementById(svg);
+    if (p.style.display == 'block')
+    {
+        p.style.display = 'none';
+        svg.style.transform ='rotate(180deg)';
+    }
+    else
+    {
+        p.style.display = 'block';
+        svg.style.transform ='rotate(0)';
+    }
+}
