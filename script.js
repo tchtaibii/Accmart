c5bf51748a26b50ea1651fd26aeaa5db5ae92a60
TEHSUS <93588863+tchtaibii@users.noreply.github.com>
2022-11-19 23:31:49 +0100

    select fix

--- script.js
+++ script.js
@@ -14,3 +14,23 @@ function ft_sc(value, svg)
         svg.style.transform ='rotate(0)';
     }
 }
+
+function getTextWidth(txt) {
+  var $elm = $('<span class="tempforSize">'+txt+'</span>').prependTo("body");
+  var elmWidth = $elm.width();
+  $elm.remove();
+  return elmWidth;
+}
+function centerSelect($elm) {
+    var optionWidth = getTextWidth($elm.children(":selected").html())
+    var emptySpace =   $elm.width()- optionWidth;
+    $elm.css("text-indent", (emptySpace/2) - 10);// -10 for some browers to remove the right toggle control width
+}
+// on start 
+$('.centerSelect').each(function(){
+  centerSelect($(this));
+});
+// on change
+$('.centerSelect').on('change', function(){
+  centerSelect($(this));
+});
